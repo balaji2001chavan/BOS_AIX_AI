b4af0a95bed14f70040bd0fad33a342de6c98a32
balaji suryakant chavhan <balaji2001chavan@gmail.com>
2025-11-21 19:27:12 +0530

    Create User.js

---
+++ backend/models/User.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true
+    },
+    passwordHash: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String
+    },
+    role: {
+      type: String,
+      default: "user"
+    }
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
